f1b24a9d2f66000613662bf8892e64746bbcf6b9
daviddev <davidtejadamoreta26@gmail.com>
2024-10-19 17:33:39 -0400

    Se a;adio la ruta de los submodulos AUTH, USER, PRODUCTS

--- services/api-gateway/fastapi-gateway/routes/api.py
+++ services/api-gateway/fastapi-gateway/routes/api.py
@@ -1,13 +1,14 @@
 # fastapi-gateway/routes/api.py
 from fastapi import APIRouter
-from .auth import router as auth_router
-from .users import router as users_router
-from .products import router as products_router
+from routes.auth.auth_routes import router_auth as auth
+from routes.users.users_routes import router_users as users
+from routes.products.products_routes import router_product as products
 
 # Crear el objeto APIRouter
 router = APIRouter()
 
 # Incluir los routers de otras rutas
-router.include_router(auth_router, prefix="/auth", tags=["auth"])
-router.include_router(users_router, prefix="/users", tags=["users"])
-router.include_router(products_router, prefix="/products", tags=["products"])
+router.include_router(auth, prefix="/auth", tags=["auth"])
+router.include_router(users, prefix="/user", tags=["user"])
+router.include_router(products, prefix="/products", tags=["products"])
+
