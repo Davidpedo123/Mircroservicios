4dd47ce2933904e7b386e19d746e7983abc70f6e
daviddev <davidtejadamoreta26@gmail.com>
2024-10-19 17:31:44 -0400

    Se incluyo uvicorn como despliegue

--- services/api-gateway/fastapi-gateway/main.py
+++ services/api-gateway/fastapi-gateway/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes.api import router as api_router
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(title="API Gateway")
 
 # Incluir el router que contiene todas las rutas
 app.include_router(api_router)
 
 @app.get("/")
-async main(request:Request):
+async def main(request:Request):
 	return {"Welcome":"The api gateway"}
 
-
+if __name__ == "__main__":
+    # Ejecutar el servidor con Uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
