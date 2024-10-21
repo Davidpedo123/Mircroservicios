7a8e9fca2b6635f787a5fcbb9f799fae1929c42b
daviddev <davidtejadamoreta26@gmail.com>
2024-10-21 12:17:47 -0400

    Se incluyo la rutas relacionadas a los servicios

--- services/api-gateway/fastapi-gateway/routes/api.py
+++ services/api-gateway/fastapi-gateway/routes/api.py
@@ -3,18 +3,19 @@ from fastapi import APIRouter, FastAPI, Request
 from routes.auth.auth_routes import router_auth as auth
 from routes.users.users_routes import router_users as users
 from routes.products.products_routes import router_product as products
+from routes.service.routing_manager import manager_routing as service1
 
-# Crear el objeto APIRouter
 router = APIRouter()
 
 services = FastAPI()
 
 
-@services.get("/services1")
-async def service1()
+#@services.get("/services1")
+#async def service1()
 
 # Incluir los routers de otras rutas
 router.include_router(auth, prefix="/auth", tags=["auth"])
 router.include_router(users, prefix="/user", tags=["user"])
 router.include_router(products, prefix="/products", tags=["products"])
+router.include_router(service1, prefix="/service", tags=["service"])
 
