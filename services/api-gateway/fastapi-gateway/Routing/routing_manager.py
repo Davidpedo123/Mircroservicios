56a9e61cbbd1624e7bb498892c1f050d452dd37a
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:11:10 -0400

    clean code

--- services/api-gateway/fastapi-gateway/Routing/routing_manager.py
+++ services/api-gateway/fastapi-gateway/Routing/routing_manager.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, Request
 from service_example import hellowork 
 import httpx
-  # Importa la lógica del login
+
 from fastapi import Depends
 
 manager_routing = APIRouter()
 
-# Define la ruta para el login
+
 @manager_routing.get("/service1")
 def service1(request: Request):
     return hellowork()
