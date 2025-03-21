91cf07ba92b9f21011579d138d4f3ffd5b278659
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:15:08 -0400

    clean code

--- services/api-gateway/fastapi-gateway/routes/load_balancer/round_robin.py
+++ services/api-gateway/fastapi-gateway/routes/load_balancer/round_robin.py
@@ -4,16 +4,16 @@
 class RoundRobin:
     def __init__(self, servers):
         self.servers = servers
-        self.counter = 0  # Inicializa el contador
+        self.counter = 0 
 
     def get_next_server(self):
-        # Obtiene el servidor actual basado en el contador
+        
         selected_server = self.servers[self.counter]
 
-        # Incrementa el contador
+        
         self.counter += 1
         
-        # Reinicia el contador si ha superado el número de servidores
+        
         if self.counter >= len(self.servers):
             self.counter = 0
             
