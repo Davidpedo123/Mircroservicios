3d8dd6d35cd216b07544fdb83f526365dcd65919
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:21:38 -0400

    clean code

--- services/api-gateway/fastapi-gateway/error/not_found.py
+++ services/api-gateway/fastapi-gateway/error/not_found.py
@@ -1,11 +1,11 @@
-# error/not_found.py
+
 from fastapi import Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
 
-# Manejador de excepción HTTP
+
 async def http_exception_handler(request: Request, exc: HTTPException):
     if exc.status_code == 404:
         return templates.TemplateResponse("404.html", {"request": request})
