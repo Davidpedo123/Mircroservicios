379437b296d8c170b5608e780968a866856612da
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:09:10 -0400

    clean code

--- services/api-gateway/fastapi-gateway/models/user.py
+++ services/api-gateway/fastapi-gateway/models/user.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, Field
 
 class Users(SQLModel, table=True):
-    __tablename__ = "users"  # Esto define explícitamente el nombre de la tabla
+    __tablename__ = "users"  
 
     id: int = Field(default=None, primary_key=True)
     username: str = Field(index=True)
