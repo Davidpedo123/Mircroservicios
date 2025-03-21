9c1a183870ebf687d99fa107be60bd049ab7fac4
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:18:54 -0400

    clean code

--- services/api-gateway/fastapi-gateway/routes/cache/cache_manager.py
+++ services/api-gateway/fastapi-gateway/routes/cache/cache_manager.py
@@ -11,17 +11,16 @@ class CacheMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         cache_key = request.url.path
         
-        # Verificar si la respuesta está en caché
         if cache_key in self.cache:
             return self.cache[cache_key]
 
-        # Llamar al siguiente middleware o al controlador
+        
         response = await call_next(request)
 
-        # Almacenar la respuesta en caché
+        
         self.cache[cache_key] = response
 
-        # Modificar los encabezados después de la respuesta
+        
         response.headers["Cache-Control"] = "max-age=15"
 
         return response
