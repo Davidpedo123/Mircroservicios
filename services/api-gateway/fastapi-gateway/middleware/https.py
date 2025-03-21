1e2355dbbf44c6218142dd73e598c1780b7411b4
Eduardo David Tejada Moreta <105448798+Davidpedo123@users.noreply.github.com>
2025-03-21 08:08:25 -0400

    clean code

--- services/api-gateway/fastapi-gateway/middleware/https.py
+++ services/api-gateway/fastapi-gateway/middleware/https.py
@@ -3,9 +3,9 @@ from starlette.middleware.base import BaseHTTPMiddleware
 
 class RedirectHTTPToHTTPSMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        # Verifica si la solicitud es HTTP
+        
         if request.url.scheme == "http":
-            # Redirige a HTTPS
+            
             https_url = request.url.replace(scheme="https")
             return Response(status_code=301, headers={"Location": str(https_url)})
         
